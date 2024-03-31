89b9f62a4c28f5a57e57fca09c39e00859fe49b5
wolaoer <2933140094@qq.com>
2024-03-31 17:25:07 +0800

    1

--- src/main/java/com/studio314/d_emo/mapper/ChatsMapper.java
+++ src/main/java/com/studio314/d_emo/mapper/ChatsMapper.java
@@ -4,7 +4,10 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.studio314.d_emo.pojo.Chats;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Mapper
 public interface ChatsMapper extends BaseMapper<Chats>{
@@ -13,4 +16,7 @@ public interface ChatsMapper extends BaseMapper<Chats>{
 
     @Insert("insert into chats(senderID, message, type, isReceiver, audioTime) values (#{senderID}, #{message}, #{type}, #{isReceiver}, #{audioTime})")
     void insertChatWithAudio(int senderID, String message, int type, int isReceiver, int audioTime);
+
+    @Select("select emotion from chats where senderID = #{senderID} and DATE(sendTime) = CURDATE();")
+    List<Chats> getCurDayEmotion(int senderID);
 }
